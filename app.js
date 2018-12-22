04929443bcf6f144279c8b1246d61816e754dccd
Heet1996 <heet1885@gmail.com>
2018-12-22 23:08:13 +0530

    file feature added

--- app.js
+++ app.js
@@ -2,23 +2,32 @@ const http=require('http');
 const fs=require('fs');
 //Below function will register in event loop and returns a server
 const server= http.createServer((req,res)=>{
-    console.log(req);
+    
     // process.exit()  //To unregister from event looop .
     const method=req.method;
     const url=req.url;
     if(url==='/'){
     res.setHeader('Content-type','text/html');
     res.write('<html>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="button">Submit</button></form></body>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>')
     res.write('</html>');
     return res.end();
     }
-    else if(url==='/message' && method==='POST')
-    {
-        res.writeFileSync('message.txt','Dummy');
+     if(url==='/message' && method==='POST')
+    {   const body=[];
+        //Creating an event whenever data arrives
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFileSync('message.txt',message) ;
+        });
         res.statusCode=302;
         res.setHeader('Location','/');
-        return res.header();
+        return res.end();
     }
 
 });
