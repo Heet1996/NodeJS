b13ae12e1b2d76cda4f9f3f71327a7d01b8cfbe6
Heet1996 <heet1885@gmail.com>
2019-01-02 20:30:05 +0530

    middleware inserted

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http=require('http');
-const requestHandler=require('./route');
+// const requestHandler=require('./route');
+
+const express=require('express');
+const app=express();
+
 //Below function will register in event loop and returns a server
-const server= http.createServer(requestHandler(req,res));
+// const server= http.createServer(requestHandler(req,res));
+
+
+//Registering for middleware
+app.use((req,res,next)=>{
+    res.send("<h1>Hello World</h1>");
+    next(); //Goto next middleware
+});
+const server= http.createServer(app);
 //Sever is listen at this port
 server.listen('3000');
--- route.js
+++ route.js
@@ -1,30 +1,30 @@
-    const fs=require('fs');
-   // process.exit()  //To unregister from event looop .
-    const requestHandler=(req,res)=>{
-    const method=req.method;
-    const url=req.url;
-    if(url==='/'){
-    res.setHeader('Content-type','text/html');
-    res.write('<html>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>')
-    res.write('</html>');
-    return res.end();
-    }
-     if(url==='/message' && method==='POST')
-    {   const body=[];
-        //Creating an event whenever data arrives
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString();
-            const message=parseBody.split('=')[1];
-            fs.writeFileSync('message.txt',message) ;
-        });
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-   }
-   module.exports=requestHandler;
+//     const fs=require('fs');
+//    // process.exit()  //To unregister from event looop .
+//     const requestHandler=(req,res)=>{
+//     const method=req.method;
+//     const url=req.url;
+//     if(url==='/'){
+//     res.setHeader('Content-type','text/html');
+//     res.write('<html>');
+//     res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>')
+//     res.write('</html>');
+//     return res.end();
+//     }
+//      if(url==='/message' && method==='POST')
+//     {   const body=[];
+//         //Creating an event whenever data arrives
+//         req.on('data',(chunk)=>{
+//             console.log(chunk);
+//             body.push(chunk);
+//         });
+//         req.on('end',()=>{
+//             const parseBody=Buffer.concat(body).toString();
+//             const message=parseBody.split('=')[1];
+//             fs.writeFileSync('message.txt',message) ;
+//         });
+//         res.statusCode=302;
+//         res.setHeader('Location','/');
+//         return res.end();
+//     }
+//    }
+//    module.exports=requestHandler;
