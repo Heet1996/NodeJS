c7ca7c1e62633c18b59626828a8712ce0de68d45
Heet1996 <heet1885@gmail.com>
2019-01-05 23:22:46 +0530

    Added filter routes

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const http=require('http');
 const express=require('express');
 const app=express();
 
+const bodyParser=require('body-parser');
+const adminRouter=require('./routes/admin');
+const shopRouter=require('./routes/shop');
 //Below function will register in event loop and returns a server
 // const server= http.createServer(requestHandler(req,res));
 
+//Reistering body-parser
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Router for admins
+app.use('/admin',adminRouter);
 //Registering for middleware
-app.use((req,res,next)=>{
-    res.send("<h1>Hello World</h1>");
-    next(); //Goto next middleware
-});
+//Router for users
+app.use(shopRouter);
 // const server= http.createServer(app);
 //Sever is listen at this port
 app.listen('3000');
