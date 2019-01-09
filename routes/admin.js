9c89467ffd8b9d03eeded37955c2d0ac9de2566a
Heet1996 <heet1885@gmail.com>
2019-01-09 21:19:44 +0530

    view folder added

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path=require('path');
+
 const express=require('express');
 const router=express.Router();
 
 router.get('/add-product',(req,res)=>{
-    res.send("<form action='/admin/product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>")
+    res.sendFile(path.join(__dirname,"../","views","add-product.html"));
 });
-router.post('/product',(req,res)=>{
+router.post('/add-product',(req,res)=>{
     console.log(req.body);
     res.redirect("/");
 });
