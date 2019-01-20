4a085411c9c312c22907f5e3a7b4a0e1fe25ef93
Heet1996 <heet1885@gmail.com>
2019-01-20 10:17:24 +0530

    added layput template

--- routes/pageError.js
+++ routes/pageError.js
@@ -4,6 +4,6 @@ const express=require('express');
 var router=express.Router();
 
 router.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,"../","views","page-error.html"));
+    res.status(404).render("page-error");
 });
 module.exports=router;
