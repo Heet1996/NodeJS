39804d2f4651625ff87dc90d3eed277015abff68
Heet Shah <heet1885@gmail.com>
2019-02-19 18:55:16 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const connection=mysql.createPool({
     host:'localhost',
     user:'root',
     database:'ShoppingCart',
-    password:'23rdmay1996'
+    password:'*********'
 });
 
 module.exports=connection.promise();
